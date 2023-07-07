a14307f48a60db25fcb50fc3efe5a866dbf26a5f
Sven Moj <sven_moj@yahoo.de>
2023-07-07 13:52:55 +0200

    set timeout for push notification

--- examples/web-push/pages/api/notification.js
+++ examples/web-push/pages/api/notification.js
@@ -10,23 +10,27 @@ const Notification = (req, res) => {
   if (req.method == 'POST') {
     const { subscription } = req.body
 
-    webPush
-      .sendNotification(
-        subscription,
-        JSON.stringify({ title: 'Hello Web Push', message: 'Your web push notification is here!' })
-      )
-      .then(response => {
-        res.writeHead(response.statusCode, response.headers).end(response.body)
-      })
-      .catch(err => {
-        if ('statusCode' in err) {
-          res.writeHead(err.statusCode, err.headers).end(err.body)
-        } else {
-          console.error(err)
-          res.statusCode = 500
-          res.end()
-        }
-      })
+    setTimeout(() => {
+
+
+      webPush
+        .sendNotification(
+          subscription,
+          JSON.stringify({ title: 'Hello Web Push', message: 'Your web push notification is here!' })
+        )
+        .then(response => {
+          res.writeHead(response.statusCode, response.headers).end(response.body)
+        })
+        .catch(err => {
+          if ('statusCode' in err) {
+            res.writeHead(err.statusCode, err.headers).end(err.body)
+          } else {
+            console.error(err)
+            res.statusCode = 500
+            res.end()
+          }
+        })
+    }, 5000)
   } else {
     res.statusCode = 405
     res.end()
