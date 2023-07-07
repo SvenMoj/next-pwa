5812b2f09d172f2b3dd061d32fb2d1a23ef07067
Sven Moj <sven_moj@yahoo.de>
2023-07-07 14:21:06 +0200

    updateding api/notifications

--- examples/web-push/pages/api/notification.js
+++ examples/web-push/pages/api/notification.js
@@ -26,7 +26,28 @@ const Notification = (req, res) => {
           res.end()
         }
       })
-  } else {
+  }
+  if (req.method == 'GET') {
+    const { subscription } = req.body
+    webPush
+      .sendNotification(
+        subscription,
+        JSON.stringify({ title: 'Hello Cron', message: 'This might be annoying!' })
+      )
+      .then(response => {
+        res.writeHead(response.statusCode, response.headers).end(response.body)
+      })
+      .catch(err => {
+        if ('statusCode' in err) {
+          res.writeHead(err.statusCode, err.headers).end(err.body)
+        } else {
+          console.error(err)
+          res.statusCode = 500
+          res.end()
+        }
+      })
+  }
+  else {
     res.statusCode = 405
     res.end()
   }
